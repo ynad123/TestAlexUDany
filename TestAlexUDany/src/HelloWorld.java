73a724d25df04a1c7d17932dcb329d2bf5778ae7
AlpenJodler <dany.meyer@hnu.de>
2023-01-16 12:18:07 +0100

    Log4j-Beispiel integriert

--- TestAlexUDany/src/HelloWorld.java
+++ TestAlexUDany/src/HelloWorld.java
@@ -1,8 +1,23 @@
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 public class HelloWorld {
+	private static Logger logger = LogManager.getLogger(HelloWorld.class);
+	
 
 	public static void main(String[] args) {
 		System.out.println("huhu");
+		
+		Configurator.setRootLevel(Level.ERROR);
+		logger.trace("Trace Message!");
+		logger.debug("Debug Message!");
+		logger.info("Info Message!");
+		logger.warn("Warn Message!");
+		logger.error("Error Message!");
+		logger.fatal("Fatal Message!");
+		
 
 	}
 
