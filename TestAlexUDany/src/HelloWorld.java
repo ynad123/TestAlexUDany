6dd673ebba7e35468e0a3a370264fb9780fb2bb9
AlpenJodler <dany.meyer@hnu.de>
2023-01-16 19:18:20 +0100

    add coomment in code

--- TestAlexUDany/src/HelloWorld.java
+++ TestAlexUDany/src/HelloWorld.java
@@ -3,6 +3,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
 
+//kleine Aenderung als Kommentar
 public class HelloWorld {
 	private static Logger logger = LogManager.getLogger(HelloWorld.class);
 	
